4ac2f5d18430fec8fc79859ea2b22b591abf4f93
ComplexPlane <complexplanegames@gmail.com>
2021-07-01 20:52:31 -0400

    Use gson library to output DME watchlist
    Can now see integer and struct memory values (with incorrect addresses)

--- src/main/java/supermonkeyballtools/DmeWatchList.java
+++ src/main/java/supermonkeyballtools/DmeWatchList.java
@@ -1,5 +1,7 @@
 package supermonkeyballtools;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import ghidra.program.model.address.Address;
 import ghidra.program.model.data.*;
 import ghidra.program.model.listing.Data;
@@ -14,35 +16,65 @@ import java.util.List;
 
 public class DmeWatchList {
 
+    private static class WatchList {
+        private List<Object> watchList;
+
+        public WatchList(List<Object> watchList) {
+            this.watchList = watchList;
+        }
+    }
+
+    private static class VarWatch {
+        private String address;
+        private int baseIndex;
+        private String label;
+        private int typeIndex;
+        private boolean unsigned;
+
+        public VarWatch(String address, int baseIndex, String label, int typeIndex, boolean unsigned) {
+            this.address = address;
+            this.baseIndex = baseIndex;
+            this.label = label;
+            this.typeIndex = typeIndex;
+            this.unsigned = unsigned;
+        }
+    }
+
+    private static class GroupWatch {
+        private List<Object> groupEntries;
+        private String groupName;
+
+        public GroupWatch(List<Object> groupEntries, String groupName) {
+            this.groupEntries = groupEntries;
+            this.groupName = groupName;
+        }
+    }
+
     /*
     TODO
-    Remember to delete comma
-        Deal with commas in general
     Convert addresses to actual GC REL addresses
-    Deal with undefined
-    Export the different sizes of integers
      */
 
     private enum TypeIndex {
         BYTE, SHORT, WORD, FLOAT,
     }
 
-    private static void genStruct(Structure structType, Address addr, List<String> outLines) {
-        outLines.add("{");
-        outLines.add("\"groupEntries\": [");
+    private static GroupWatch genStruct(String name, Structure structType, Address addr) {
+        List<Object> groupEntries = new ArrayList<>();
 
         for (int i = 0; i < structType.getNumComponents(); i++) {
             DataTypeComponent compon = structType.getComponent(i);
             Address fieldAddr = addr.add(compon.getOffset());
-            genDataType(compon.getDataType(), compon.getFieldName(), fieldAddr, outLines);
+            Object fieldWatch = genDataType(compon.getFieldName(), compon.getDataType(), fieldAddr);
+            if (fieldWatch != null) {
+                groupEntries.add(fieldWatch);
+            }
         }
 
-        outLines.add("],");
-        outLines.add(String.format("\"groupName\": \"%s\"", structType.getName()));
-        outLines.add("},");
+        return new GroupWatch(groupEntries, name);
     }
 
-    private static void genDataType(DataType type, String name, Address addr, List<String> outLines) {
+    private static Object genDataType(String name, DataType type, Address addr) {
         if (type instanceof TypeDef) {
             type = ((TypeDef) type).getBaseDataType();
         }
@@ -56,32 +88,26 @@ public class DmeWatchList {
             } else if (type.getLength() == 4) {
                 ti = TypeIndex.WORD;
             } else {
-                return; // Don't support an integer of this unknown size
+                return null; // Don't support an integer of this unknown size
             }
             boolean signed = true;
             if (type instanceof AbstractIntegerDataType) {
                 signed = ((AbstractIntegerDataType) type).isSigned();
             }
 
-            outLines.add("{");
-            outLines.add(String.format("\"address\": \"%s\",", addr.toString()));
-            outLines.add("\"baseIndex\": 0,");
-            outLines.add(String.format("\"label\": \"%s\",", name));
-            outLines.add(String.format("\"typeIndex\": %d,", ti.ordinal()));
-            outLines.add(String.format("\"unsigned\": %b", signed));
-            outLines.add("},");
-
-        } else if (type instanceof Structure) {
-            genStruct((Structure) type, addr, outLines);
+            return new VarWatch(addr.toString(), 0, name, ti.ordinal(), !signed);
         }
+        if (type instanceof Structure) {
+            return genStruct(name, (Structure) type, addr);
+        }
+        return null;
     }
 
     public static String genDmeWatchList(Program program) {
-        // TODO use some sort of string buffer?
-        List<String> lines = new ArrayList<>();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-        lines.add("{");
-        lines.add("\"watchList\": [");
+
+        List<Object> watchList = new ArrayList<>();
 
         SymbolTable table = program.getSymbolTable();
         for (Iterator<Symbol> it = table.getSymbolIterator(); it.hasNext(); ) {
@@ -92,12 +118,12 @@ public class DmeWatchList {
             if (data == null) continue;
 
             DataType type = data.getDataType();
-            genDataType(type, s.getName(), data.getAddress(), lines);
+            Object watch = genDataType(s.getName(), type, data.getAddress());
+            if (watch != null) {
+                watchList.add(watch);
+            }
         }
 
-        lines.add("]");
-        lines.add("}");
-
-        return String.join("\n", lines);
+        return gson.toJson(new WatchList(watchList));
     }
 }
