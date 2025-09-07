a59ede4432d255e4833b0f98afef19c199aa3217
ComplexPlane <46565903+ComplexPlane@users.noreply.github.com>
2025-09-07 10:13:26 -0400

    Update extensionl.properties and install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,12 @@
 set -e
 
 export GHIDRA_INSTALL_DIR=$HOME/build/ghidra_11.4.1_PUBLIC
-export JAVA_HOME=/opt/homebrew/opt/openjdk@24
+export JAVA_HOME=/opt/homebrew/opt/openjdk@21
+GHIDRA_USER_DIR=$HOME/Library/ghidra/ghidra_11.4.1_PUBLIC
 
+rm -rf dist
 ./gradlew
-rm -rf $HOME/Library/ghidra/ghidra_11.4.1_PUBLIC/Extensions/SuperMonkeyBallTools/
-unzip dist/ghidra_11.4.1_* -d $HOME/Library/ghidra/ghidra_11.4.1_PUBLIC/Extensions/
+rm -rf $GHIDRA_USER_DIR/Extensions/SuperMonkeyBallTools/
+rm -rf $GHIDRA_INSTALL_DIR/Extensions/SuperMonkeyBallTools/
+unzip dist/ghidra_11.4.1_* -d $GHIDRA_USER_DIR/Extensions
+
